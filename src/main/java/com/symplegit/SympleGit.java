c09bf27d56a524ac5cda6586388dd85f380c7b1d
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2024-01-08 16:03:55 +0100

    SympleGit: add useStringOutput

--- src/main/java/com/symplegit/SympleGit.java
+++ src/main/java/com/symplegit/SympleGit.java
@@ -15,12 +15,15 @@ import java.util.Objects;
 public class SympleGit {
 
     private File projectDir;
+    private boolean useStringOutput = false;
 
     /**
      * Constructs a SympleGit instance for a given project directory. This
      * constructor ensures that the provided directory exists and is indeed a
-     * directory.
-     *
+     * directory.<br>
+     * The output of each Git command will be stored in a temporary File. The
+     * temporary file will be deleted after the JVM's life.
+     * 
      * @param projectDir The directory of the Git project.
      * @throws FileNotFoundException if the project directory does not exist or is
      *                               not a directory.
@@ -35,12 +38,49 @@ public class SympleGit {
     }
 
     /**
+     * Constructs a SympleGit instance for a given project directory. This
+     * constructor ensures that the provided directory exists and is indeed a
+     * directory.
+     *
+     * @param projectDir      The directory of the Git project.
+     * @param useStringOutput If true, the output of the Git commands will be only
+     *                        stored in a String. if false, the output of each Git
+     *                        command will be stored in a temporary File. The
+     *                        temporary file will be deleted after the JVM's life.
+     * 
+     * @throws FileNotFoundException if the project directory does not exist or is
+     *                               not a directory.
+     * @throws NullPointerException  if the projectDir argument is null.
+     */
+    public SympleGit(File projectDir, boolean useStringOutput) throws FileNotFoundException {
+	this.projectDir = Objects.requireNonNull(projectDir, "projectDir cannot be null!");
+	this.useStringOutput = useStringOutput;
+
+	if (!projectDir.isDirectory()) {
+	    throw new FileNotFoundException("The project does not exist: " + projectDir);
+	}
+    }
+
+    /**
      * Retrieves the project directory associated with this SympleGit instance.
      *
      * @return The File object representing the project directory.
      */
     public File getProjectDir() {
 	return projectDir;
+    }
+
+    /**
+     * Says if the output of the Git commands will be stored in a String. If true,
+     * the output of the Git commands will be stored in a String, if false, the
+     * output of each Git command will be stored in a temporary File. The temporary
+     * file will be deleted after the JVM's life.
+     * 
+     * @return true if the output of the Git commands will be stored in a String,
+     *         false otherwise.
+     */
+    public boolean isUseStringOutput() {
+	return useStringOutput;
     }
 
 }
