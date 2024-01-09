8ce9c770bb0513ad4874b0d37d7327be1b07341d
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2024-01-09 16:58:17 +0100

    Add licens file

--- src/main/java/com/symplegit/GitWrapper.java
+++ src/main/java/com/symplegit/GitWrapper.java
@@ -1,5 +1,28 @@
+/*
+ * This file is part of SympleGit. 
+ * SympleGit: Git in Java for the rest of us                                     
+ * Copyright (C) 2024,  KawanSoft SAS
+ * (http://www.kawansoft.com). All rights reserved.                                
+ *                                                                               
+ * SympleGit is free software; you can redistribute it and/or                 
+ * modify it under the terms of the GNU Lesser General Public                    
+ * License as published by the Free Software Foundation; either                  
+ * version 2.1 of the License, or (at your option) any later version.            
+ *                                                                               
+ * SympleGit is distributed in the hope that it will be useful,               
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU             
+ * Lesser General Public License for more details.                               
+ *                                                                               
+ * You should have received a copy of the GNU Lesser General Public              
+ * License along with this library; if not, write to the Free Software           
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  
+ * 02110-1301  USA
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ */
 package com.symplegit;
-
 /**
  * The GitWrapper interface defines the essential functionalities that a wrapper
  * class for Git operations should implement. This interface ensures a
@@ -10,7 +33,6 @@ package com.symplegit;
  * that may have occurred.
  */
 public interface GitWrapper {
-
     /**
      * Checks whether the last executed Git command completed successfully.
      *
@@ -18,7 +40,6 @@ public interface GitWrapper {
      *         0), false otherwise.
      */
     public boolean isResponseOk();
-
     /**
      * Retrieves the error message from the last executed Git command, if any.
      *
@@ -26,7 +47,6 @@ public interface GitWrapper {
      *         execution. Returns null or an empty string if there was no error.
      */
     public String getError();
-
     /**
      * Gets the exception that was thrown during the last executed Git command, if
      * any.
@@ -36,5 +56,4 @@ public interface GitWrapper {
      *         thrown.
      */
     public Exception getException();
-
 }
