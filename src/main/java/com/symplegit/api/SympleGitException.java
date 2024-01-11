188185399011aa0c618abb9aa5f0f9b75f6a43c6
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2024-01-11 17:34:38 +0100

    remove SympleGitException

--- src/main/java/com/symplegit/api/SympleGitException.java
+++
@@ -1,9 +0,0 @@
-package com.symplegit.api;
-
-public class SympleGitException extends Exception {
-
-    public SympleGitException(String string) {
-	// TODO Auto-generated constructor stub
-    }
-
-}
